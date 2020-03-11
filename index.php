07ec538e50a23678d3105bda8265c21007e1e97f
yuri-1984 <yuri.hiraoka@rakus-partners.co.jp>
2020-03-11 17:34:03 +0900

    delete password

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ $res = null;
 $pdo = null;
 
 try {
-    $pdo = new PDO('mysql:dbname=mysql;host=localhost;charset=utf8;unix_socket=/tmp/mysql.sock','root', '0427yuri');
+    $pdo = new PDO('mysql:dbname=mysql;host=localhost;charset=utf8;unix_socket=/tmp/mysql.sock','root', '******');
 
     $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES,true);
